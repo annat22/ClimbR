14849189f8824e2de91b48a8818ae1beef48987d
annat22 <annat22@gmail.com>
2025-06-09 10:23:24 -0400

    add climbGETjoin

---
+++ climbGETjoin.R
@@ -0,0 +1,25 @@
+## get animal metadata from animals facet and joins it with provided data table
+## input is data table with 'a'animalName' column with animal names as on climb
+## animalName is not case sensitive
+## Returns a table of the original data joined with animal metadata
+## animal metadata includes
+
+library(tidyverse)
+source("https://raw.github.com/annat22/ClimbR/main/climbGETdf.R")
+
+climbGETbde <- function(datatable) {
+  
+  datatable <- datatable %>%
+    mutate(animalName = toupper(animalName))
+  
+  # get animal name, climb ID, and other info
+  animals <- climbGETdf("animals") %>%
+    select(animalName, "climbID"=animalId, dateBorn, dateExit, sex, use, line) %>%
+    mutate(across(matches("date"), as.Date))
+  
+
+  # put it together
+  df <- right_join(animals, datatable) 
+
+  return(df)
+  }
